ca2f649ca29435b733bad72e9ce6691ea6a4bdf3
Tailor Vishwa <tailorvishwa3105@gmail.com>
2024-07-11 20:43:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -40,12 +40,12 @@ function App() {
   return (
     <>
       <Router>
-        <Navbar tital="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Navbar tital="Textcraft" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
           {/*<TextForm showAlert={showAlert} heading="Enter the Text below to Analyze" mode={mode} />*/}
           <Routes>
-            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="TextUtils - Word Counter, Character Counter, Remove extra Spaces" mode={mode} />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Textcraft - Word Counter, Character Counter, Remove extra Spaces" mode={mode} />} />
             <Route exact path="/about" element={<About mode={mode} />} />
           </Routes>
         </div>
